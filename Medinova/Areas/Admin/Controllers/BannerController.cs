56d42c71f1c19147751d42bc973cc1ddc2e44ed8
Burak Han Ulusoy <burakhanulusoy18@gmail.com>
2026-02-19 23:20:33 +0300

    admin banners finished 23:20  19.02.2026

---
+++ Medinova/Areas/Admin/Controllers/BannerController.cs
@@ -0,0 +1,96 @@
+﻿using Medinova.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Medinova.Areas.Admin.Controllers
+{
+    public class BannerController : Controller
+    {
+        MedinovaContext _context = new MedinovaContext();
+
+        // GET: Admin/Banner
+        public ActionResult Index()
+        {
+            var banners = _context.Banners.ToList();
+            return View(banners);
+        }
+
+        public ActionResult DeleteBanner(int id)
+        {
+
+            var banner = _context.Banners.Find(id);
+            _context.Banners.Remove(banner);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult CreateBanner()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult CreateBanner(Banner  banner)
+        {
+            _context.Banners.Add(banner);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+
+        [HttpGet]
+        public ActionResult UpdateBanner(int id)
+        {
+
+            var banner = _context.Banners.Find(id);
+            return View(banner);
+
+        }
+
+        [HttpPost]
+        public ActionResult UpdateBanner(Banner banner)
+        {
+
+            _context.Entry(banner).State = EntityState.Modified;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
